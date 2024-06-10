b46823bf8363a8bc53a96526cd0c0d21f302d3be
Jeova Junior <jrjeova@hotmail.com>
2024-06-10 19:50:08 -0300

    feature: Fazendo filtro de treinadores na tela principal

--- src/components/MainDashboard/MainDashboard.tsx
+++ src/components/MainDashboard/MainDashboard.tsx
@@ -15,7 +15,7 @@ const MainDashboard = () => {
   const [activeClients, setActiveClients] = useState<activeClientsProps | null>(
     null
   );
-  const [loading,setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
   const [schedule, setSchedule] = useState({});
 
   const getTrainers = async () => {
@@ -29,46 +29,42 @@ const MainDashboard = () => {
     }
   };
 
-  const fetchSchedule = async () =>{
+  const fetchSchedule = async () => {
     const response = await api.get("/client/daysTrainner");
     setSchedule(response?.data?.response?.response);
-  }
+  };
 
   useEffect(() => {
-       // Função para buscar treinadores e clientes ativos
-       const fetchData = async () => {
-        try {
-          await Promise.all([getTrainers(), getActiveClients(),fetchSchedule()]); // Espera ambas as solicitações serem completadas
-          setLoading(false); // Define o estado de carregamento como false quando os dados são carregados
-        } catch (error) {
-          console.error("Erro ao buscar dados:", error);
-        }
-      };
-  
-      fetchData(); // Chama a função para buscar os dados
-    }, []);
- 
+    // Função para buscar treinadores e clientes ativos
+    const fetchData = async () => {
+      try {
+        await Promise.all([getTrainers(), getActiveClients(), fetchSchedule()]); // Espera ambas as solicitações serem completadas
+        setLoading(false); // Define o estado de carregamento como false quando os dados são carregados
+      } catch (error) {
+        console.error("Erro ao buscar dados:", error);
+      }
+    };
 
-
+    fetchData(); // Chama a função para buscar os dados
+  }, []);
 
   return (
     <div>
-      {loading &&(
+      {loading && (
         <div className="fixed inset-0 flex items-center justify-center bg-white bg-opacity-5 z-50">
-        <Loading />
-      </div>
-      )};
-      
-      <div className={`transition-filter duration-100 ${loading ? 'blur' : ''}`}>
-        <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] bg-gray-100/40 flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10 dark:bg-gray-800/40 text-black">
-        <div className="max-w-6xl w-full mx-auto grid gap-2">
-          <h1 className="font-semibold text-3xl">Dashboard</h1>
+          <Loading />
         </div>
-        <InfoComponent 
-        trainers={trainers}
-        activeClients={activeClients}
-        />
-        <CalendarRelatory schedule={schedule} />      
+      )}
+      ;
+      <div
+        className={`transition-filter duration-100 ${loading ? "blur" : ""}`}
+      >
+        <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] bg-gray-100/40 flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10 dark:bg-gray-800/40 text-black">
+          <div className="max-w-6xl w-full mx-auto grid gap-2">
+            <h1 className="font-semibold text-3xl">Dashboard</h1>
+          </div>
+          <InfoComponent trainers={trainers} activeClients={activeClients} />
+          <CalendarRelatory schedule={schedule} trainers={trainers} />
         </main>
       </div>
     </div>
--- src/components/component/calendar-relatory.tsx
+++ src/components/component/calendar-relatory.tsx
@@ -1,8 +1,8 @@
 import { AvatarFallback, Avatar } from "@/components/ui/avatar";
+import { useEffect, useState } from "react";
 
-
-export function CalendarRelatory({schedule}) {
-  
+export function CalendarRelatory({ schedule, trainers }) {
+  const [scheduelFilter, setScheduleFilter] = useState<any>([]);
   const days = [
     "Segunda",
     "Terca",
@@ -13,13 +13,58 @@ export function CalendarRelatory({schedule}) {
     "Domingo",
   ];
 
+  useEffect(() => {
+    schedule && setScheduleFilter(schedule);
+  }, [schedule]);
 
+  const onSelectFilter = (trainerId: string = "") => {
+    if (trainerId === "all") {
+      setScheduleFilter(schedule);
+      return schedule;
+    }
+    if (trainerId) {
+      return Object.keys(schedule).reduce((acc, day) => {
+        const filtered = Object.keys(schedule[day]).reduce((acc, time) => {
+          const clients = schedule[day][time].filter((client) => {
+            return client?.trainer?.id == trainerId;
+          });
+
+          if (clients?.length > 0) {
+            acc[time] = clients;
+          }
+
+          return acc;
+        }, {});
+
+        if (Object.keys(filtered).length > 0) {
+          acc[day] = filtered;
+        }
+        setScheduleFilter(acc);
+        return acc;
+      }, {});
+    }
+    setScheduleFilter(schedule);
+    return schedule;
+  };
 
   return (
     <section className="max-w-6xl w-full mx-auto grid gap-6">
       <div className="container">
-        <div className="max-w-6xl w-full mx-auto grid gap-2">
+        <div className="max-w-6xl flex flex-row w-full gap-2 justify-between">
           <h5 className="font-semibold text-3xl">Calendario Semanal</h5>
+          <select
+            onChange={(e) => {
+              onSelectFilter(e.target.value);
+            }}
+            className="w-1/4 bg-white p-2 border border-gray-200 rounded-lg"
+          >
+            <option value="all">Todos</option>
+            {trainers.map((trainer) => (
+              <option key={trainer.id} value={trainer.id}>
+                {trainer?.name}
+              </option>
+            ))}
+          </select>
         </div>
         <div className="mt-10 grid gap-6 md:grid-cols-2 lg:grid-cols-3">
           {days.map((day) => (
@@ -28,20 +73,20 @@ export function CalendarRelatory({schedule}) {
               className="rounded-lg border border-gray-200 p-6 shadow-sm dark:border-gray-800"
             >
               <h3 className="text-lg font-semibold">{day}</h3>
-              
-              {schedule[day] && Object.keys(schedule[day]).length > 0 ? (
-                Object.keys(schedule[day])
+
+              {scheduelFilter[day] &&
+              Object.keys(scheduelFilter[day]).length > 0 ? (
+                Object.keys(scheduelFilter[day])
                   .sort()
                   .map((time) => (
-                    <div key={time} className="mt-2">                      
+                    <div key={time} className="mt-2">
                       <p className="font-medium">{time}</p>
-                      
-                      {schedule[day][time].map((client) => (
+
+                      {scheduelFilter[day][time].map((client) => (
                         <div
                           key={client.id}
                           className="flex items-center gap-2 mt-2"
                         >
-                         
                           <Avatar className="w-8 h-8 bg-white">
                             <AvatarFallback>{client.name[0]}</AvatarFallback>
                           </Avatar>
@@ -54,14 +99,12 @@ export function CalendarRelatory({schedule}) {
                         </div>
                       ))}
                     </div>
-                    
                   ))
               ) : (
                 <p className="mt-4 text-sm text-gray-500 dark:text-gray-400">
                   Todos horarios disponiveis
                 </p>
               )}
-              
             </div>
           ))}
         </div>
